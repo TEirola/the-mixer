4322a18bcb1189b02d6590a4a68420e0ebabe575
TEirola <tommi.eirola@mediamonks.com>
2022-11-11 01:55:28 +0100

    Remove HomePage since dublicated with HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,131 @@
+<style src="./HomeView.scss" lang="scss"></style>
 <script setup lang="ts">
-import HomePage from "../components/pages/homePage/HomePage.vue";
+import gsap from "gsap";
+import Draggable from "gsap/Draggable";
+import InertiaPlugin from "gsap/InertiaPlugin";
+import { onMounted, ref, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import Button from "../components/atoms/button/Button.vue";
+import Heading from "../components/atoms/heading/Heading.vue";
+import CocktailsList from "../components/organisms/cocktailsList/CocktailsList.vue";
+import { useCocktailsStore } from "../stores/cocktails";
+
+gsap.registerPlugin(Draggable, InertiaPlugin);
+
+const router = useRouter();
+const route = useRoute();
+const cocktailsStore = useCocktailsStore();
+const navBackground = ref();
+const routeCategory = ref(route.query.category?.toString());
+const isOverflown = ref(false);
+
+function setCategory(category: string | null) {
+  if (category === null) return router.push({ query: { category: "" } });
+  return router.push({ query: { category: category } });
+}
+
+function animateNav() {
+  const filteredValue = document.querySelector(
+    ".nav-list-button.active"
+  ) as HTMLElement;
+
+  gsap
+    .timeline()
+    .to(".nav-list-button", {
+      color: "var(--vt-c-black)",
+      duration: 0.2,
+    })
+    .to(navBackground.value, {
+      x: filteredValue.offsetLeft,
+      width: filteredValue.offsetWidth,
+      duration: 0.2,
+    })
+    .to(
+      filteredValue,
+      {
+        color: "var(--vt-c-white)",
+        duration: 0.2,
+      },
+      "<"
+    );
+}
+
+const isNavOverflown = () => {
+  const navElement = document.querySelector(".nav-list-container");
+  console.log({ navElement });
+  if (!navElement) return false;
+
+  const { clientWidth, scrollWidth } = navElement;
+  console.log(scrollWidth > clientWidth);
+  return scrollWidth > clientWidth;
+};
+
+function getThePosition() {
+  const navElement = document.querySelector(".nav-list-container");
+  if (-(navElement as any)?._gsMaxScrollX === this.endX)
+    isOverflown.value = false;
+  else {
+    isOverflown.value = true;
+  }
+}
+
+watch(route, (value) => {
+  routeCategory.value = value.query.category?.toString();
+
+  setTimeout(() => {
+    animateNav();
+  }, 100);
+});
+
+onMounted(() => {
+  setTimeout(() => {
+    animateNav();
+    isOverflown.value = isNavOverflown();
+  }, 100);
+
+  Draggable.create(".nav-list", {
+    bounds: ".homepage",
+    type: "x",
+    onDrag: getThePosition,
+  });
+});
+
+window.addEventListener("resize", () => {
+  isOverflown.value = isNavOverflown();
+});
 </script>
 
 <template>
   <main>
-    <HomePage />
+    <div class="homepage">
+      <Heading tag="h1" :style="'heading-1'" class="title">The Mixer</Heading>
+
+      <div
+        :class="{
+          ['nav-list-container']: true,
+          ['overflow-x']: isOverflown,
+        }"
+      >
+        <nav class="nav-list">
+          <Button
+            v-for="category, index in (cocktailsStore.categories as any).sort()"
+            :key="index"
+            type="button"
+            @click="
+              setCategory(route.query.category === category ? null : category)
+            "
+            :class="{
+              ['nav-list-button']: true,
+              ['active']: routeCategory === category,
+            }"
+          >
+            {{ category }}
+          </Button>
+          <div ref="navBackground" class="nav-background"></div>
+        </nav>
+      </div>
+
+      <CocktailsList :cocktails="(cocktailsStore.cocktails as any)" />
+    </div>
   </main>
 </template>
