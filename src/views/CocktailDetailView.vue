fd0c7e00bf3d2f8703de1c4e0b4d4d70681b1677
TEirola <tommi.eirola@mediamonks.com>
2022-11-11 01:56:43 +0100

    Improve styling on detail page and add back button

--- src/views/CocktailDetailView.vue
+++ src/views/CocktailDetailView.vue
@@ -1,11 +1,18 @@
+<style src="./CocktailDetailView.scss" lang="scss"></style>
 <script setup lang="ts">
+import gsap from "gsap";
+import ScrollToPlugin from "gsap/ScrollToPlugin";
 import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import Heading from "../components/atoms/heading/Heading.vue";
 import Image from "../components/atoms/image/Image.vue";
 import Text from "../components/atoms/text/Text.vue";
+import Pill from "../components/atoms/pill/Pill.vue";
 import IngredientsList from "../components/molecules/ingredientsList/IngredientsList.vue";
 import { useCocktailsStore } from "../stores/cocktails";
+import IconBack from "../components/atoms/icons/IconBack.vue";
+
+gsap.registerPlugin(ScrollToPlugin);
 
 const cocktailsStore = useCocktailsStore();
 const title = ref("");
@@ -14,9 +21,16 @@ const ingredients = ref([]);
 const measurements = ref([]);
 const instructions = ref("");
 const glass = ref("");
+const categories = ref<Array<string>>([]);
 const route = useRoute();
+const router = useRouter();
+
+function goBack() {
+  router.go(-1);
+}
 
 onMounted(async () => {
+  gsap.to(window, { duration: 0.4, scrollTo: 0 });
   await cocktailsStore.fetchIngredients(Number(route.params.id));
   const data = (cocktailsStore.ingredients as any).drinks[0];
   for (const key in data) {
@@ -30,12 +44,21 @@ onMounted(async () => {
   imageSrc.value = data.strDrinkThumb;
   instructions.value = data.strInstructions;
   glass.value = data.strGlass;
+  categories.value.push(data.strCategory, data.strAlcoholic);
 });
 </script>
 
 <template>
   <div class="detail-page">
+    <button type="button" class="go-back-button" @click="goBack">
+      <IconBack class="icon-back" />Back
+    </button>
     <Heading tag="h1" :style="'heading-1'">{{ title }}</Heading>
+    <div class="categories">
+      <Pill v-for="category in categories" :key="category">
+        <Text tag="p" class="category">{{ category }}</Text>
+      </Pill>
+    </div>
     <div class="detail-container">
       <Image :src="imageSrc" class="product-image" />
       <aside class="product-content">
@@ -48,32 +71,3 @@ onMounted(async () => {
     </div>
   </div>
 </template>
-
-<style>
-.detail-page {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 6rem;
-  margin-block-start: 4rem;
-}
-
-.detail-container {
-  display: flex;
-  align-items: top;
-  justify-items: space-evenly;
-  gap: 2rem;
-}
-
-.product-image {
-  max-width: 50%;
-}
-
-.product-content {
-  max-width: 50%;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-}
-</style>
